f1fe21a4b62dbfbe391f11deed75e1247a3ae843
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-07-13 17:03:08 +0300

    final

--- src/server.js
+++ src/server.js
@@ -1,33 +1,62 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
-const app = express();
+const PORT = Number(env('PORT', '3000'));
+// console.log(PORT);
+export const setupServer = () => {
+  const app = express();
+  // app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-const PORT = 3000;
-
-// app.use((req, res, next) => {
-//   next();
-// });
-app.use(cors());
-app.use(pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }));
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world!' });
-});
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: `Success!`,
+      data: contacts,
+    });
   });
-});
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
 
-app.use('*', (req, res, next) => {
-  res.status(404).json({ message: 'NOT FOUND' });
-});
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    const contact = await getContactById(contactId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id ${contactId}!',
+      data: contact,
+    });
+  });
+  app.use('*', (req, res) => {
+    res.status(404).json({ message: 'NOT FOUND' });
+  });
+  app.use((err, req, res) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
---
+++ src/services/contacts.js
@@ -0,0 +1,13 @@
+
+import { ContactsCollection } from '../db/models/contacts.js';
+
+export const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
+  return contacts;
+};
+
+export const getContactById = async (contactId) => {
+  const contact = await ContactsCollection.findById(contactId);
+//   console.log({contact});
+  return contact;
+};
