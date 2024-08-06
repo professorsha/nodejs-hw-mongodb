cd5e5fd55de3c2a94b7cee0f62235e57688e8489
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-08-06 06:16:10 +0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -2,24 +2,16 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-// import { getAllContacts, getContactById } from './services/contacts.js';
- import contactsRouter from './routers/contacts.js';
+// import contactsRouter from './routers/contacts.js';
 import router from './routers/index.js';
-import cookieParser from 'cookie-parser';
-
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
-// console.log(PORT);
 export const setupServer = () => {
   const app = express();
-
  
-  // app.use(express.json({
-  //   type: ['application/json', 'application/vnd.api+json'],
-  //   limit: '100kb',
-  // }),);
   app.use(cors());
   app.use(cookieParser());
   app.use(
@@ -29,9 +21,8 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use(contactsRouter);
-  app.use(router);
   // app.use(contactsRouter);
+  app.use(router);
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
